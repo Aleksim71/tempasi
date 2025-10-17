937d4e2fdccba6989793689dc274199ef1fc737c
Simkin <simkin@web.de>
2025-10-17 14:25:07 +0200

    chore: switch ESLint flat config to ESM (eslint.config.mjs)

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      // свои правила по вкусу
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-console": "off",
+    },
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "build/**"
+    ]
+  }
+];
