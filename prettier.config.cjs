966b72a7c55eea4a6ca37755104b9d625b5ff524
Simkin <simkin@web.de>
2025-12-05 18:44:24 +0100

    chore: update .gitignore for Tempasi

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+// prettier.config.cjs
+module.exports = {
+  printWidth: 100, // максимальная длина строки
+  tabWidth: 2, // отступ — 2 пробела
+  useTabs: false, // не использовать табы
+  semi: true, // ставить точку с запятой
+  singleQuote: true, // одинарные кавычки
+  trailingComma: "all", // запятая в конце объектов/массивов
+  bracketSpacing: true, // пробелы между скобками: { foo: bar }
+  arrowParens: "always", // всегда скобки в стрелочных функциях
+  endOfLine: "lf", // единый формат перевода строк (Linux/macOS)
+};
